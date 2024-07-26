ddc9e12e82b5611e6ae7822369a80be0c9b131c0
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-07-27 00:00:17 +0500

    remove element

---
+++ 1/#10 removeElement.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/remove-element/description/
+var removeElement = function(nums, val) {
+    const set = nums.filter((vl) => vl !== val);
+  let i = 0;
+  set.forEach((num) => {
+    nums[i] = num;
+    i++;
+  });
+  return i;
+};
