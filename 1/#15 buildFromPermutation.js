6a62340f0bd12c7358950d8ba7eb467d5a003252
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-08-01 14:39:48 +0500

    improve the code

--- 1/#15 buildFromPermutation.js
+++ 1/#15 buildFromPermutation.js
@@ -1,8 +1,8 @@
 // https://leetcode.com/problems/build-array-from-permutation/
 
 var buildArray = function(nums) {
-    let res = nums.map((num,index)=>{
-        return nums[nums[index]]
+    let res = nums.map((num)=>{
+        return nums[num]
     })
     return res
 };
