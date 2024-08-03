daa37a1d3002e011259a4e3f92fc3c906a3265a8
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-08-03 18:18:11 +0500

    timeout cancellation

---
+++ 1/#25 cancelTimeout.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/timeout-cancellation/
+
+var cancellable = function(fn, args, t) {
+    let timer = setTimeout(()=>{
+        fn(...args)
+    },t)
+    return function(){
+        clearTimeout(timer)
+    }
+};
