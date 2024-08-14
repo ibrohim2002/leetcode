1937c058a5c4c55df57db8818eeeca82b36cbed9
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-08-15 02:51:54 +0500

    #2648 Runtime 97% Memory 85%

--- 1/#84 fibGenerator.js
+++ 1/#84 fibGenerator.js
@@ -1,22 +1,21 @@
 // https://leetcode.com/problems/generate-fibonacci-sequence/
 
-// var fibGenerator = function*() {
-//     let i = 0;
-//     let num = 1;
-//     let sum = 1;
-//     yield 0;
-//     yield num;
-//     yield num;
-//     while(true){
-//         i = num
-//         num = sum
-//         sum = num + i
-//         yield sum;
-//     }
-// };
+var fibGenerator = function*() {
+    let i = 0;
+    let num = 1;
+    let sum = 1;
+    yield 0;
+    yield num;
+    yield num;
+    while(true){
+        i = num
+        num = sum
+        sum = num + i
+        yield sum;
+    }
+};
 
-// let gen = fibGenerator();
+let gen = fibGenerator();
 
-// gen.next().value;
+gen.next().value;
 
-// "#2648 Runtime 97% Memory 85%"
