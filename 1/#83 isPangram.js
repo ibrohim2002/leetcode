15e1f24880278873ef52c41deb8a9e2b2dd355f4
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-08-15 02:51:19 +0500

    #1832 Runtime 99% Memory 98%

--- 1/#83 isPangram.js
+++ 1/#83 isPangram.js
@@ -1,13 +1,12 @@
 // https://leetcode.com/problems/check-if-the-sentence-is-pangram/
 
-// var checkIfPangram = function(sentence) {
-//     let count = 0
-//     for(let i=97;i<123;i++){
-//         if(sentence.includes(String.fromCharCode(i))){
-//             count++
-//         }
-//     }
-//     return count==26
-// };
+var checkIfPangram = function(sentence) {
+    let count = 0
+    for(let i=97;i<123;i++){
+        if(sentence.includes(String.fromCharCode(i))){
+            count++
+        }
+    }
+    return count==26
+};
 
-"#1832 Runtime 99% Memory 98%"
