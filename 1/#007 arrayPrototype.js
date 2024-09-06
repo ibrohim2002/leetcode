59602a553749f25e3c0ba522d98e3fdab7e7f867
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-06 08:28:23 +0500

    #2619 Runtime 97% Memory 88%

--- 1/#007 arrayPrototype.js
+++ 1/#007 arrayPrototype.js
@@ -1,9 +1,6 @@
 // https://leetcode.com/problems/array-prototype-last/
 
 Array.prototype.last = function () {
-  if (this.length) {
-    return this[this.length - 1];
-  } else {
-    return -1;
-  }
+  if (!this.length) return -1;
+  return this[this.length - 1];
 };
