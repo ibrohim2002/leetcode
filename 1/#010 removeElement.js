8fdda41f306be3e0545ffc21360d1a1b16235bd3
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-08 23:39:56 +0500

    #27 Runtime 93% Memory 85%

--- 1/#010 removeElement.js
+++ 1/#010 removeElement.js
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/remove-element/description/
-var removeElement = function(nums, val) {
-    const set = nums.filter((vl) => vl !== val);
+var removeElement = function (nums, val) {
+  const set = nums.filter((vl) => vl !== val);
   let i = 0;
   set.forEach((num) => {
     nums[i] = num;
