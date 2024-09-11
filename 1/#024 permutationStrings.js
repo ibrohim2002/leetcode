a19babc36be30cc270688af9fb3cc5c687b4790c
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-11 17:16:49 +0500

    #3146 Runtime 92% Memory 90%

---
+++ 1/#024 permutationStrings.js
@@ -0,0 +1,9 @@
+//  https://leetcode.com/problems/permutation-difference-between-two-strings/
+
+var findPermutationDifference = function (s, t) {
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+    res += Math.abs(i - t.indexOf(s[i]));
+  }
+  return res;
+};
--- 1/#24 permutationStrings.js
+++
@@ -1,9 +0,0 @@
-//  https://leetcode.com/problems/permutation-difference-between-two-strings/
-
-var findPermutationDifference = function(s, t) {
-    let res = 0
-    for(let i=0; i<s.length; i++){
-        res += Math.abs(i-t.indexOf(s[i]))
-    }
-    return res
-};
