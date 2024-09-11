95109aa32bb7e374bbd6b2e822212e81447a9ba6
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-11 12:33:34 +0500

    #1512 Runtime 97% Memory 99%

---
+++ 1/#018 goodPairs.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/number-of-good-pairs/
+
+var numIdenticalPairs = function (nums) {
+  let res = 0;
+  nums.forEach((num, i) => {
+    nums.forEach((n, j) => {
+      num === nums[j + i + 1] && res++;
+    });
+  });
+  return res;
+};
