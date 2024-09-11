0a8961463dbef0625f9ce3df5fda43a6e65f7d8d
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-11 12:53:15 +0500

    #2942 Runtime 97% Memory 90%

--- 1/#020 findChar.js
+++ 1/#020 findChar.js
@@ -1,6 +1,9 @@
 // https://leetcode.com/problems/find-words-containing-character/
+
 var findWordsContaining = function (words, x) {
   let res = [];
-  words.forEach((word, i) => word.includes(x) && res.push(i));
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].includes(x)) res.push(i);
+  }
   return res;
 };
