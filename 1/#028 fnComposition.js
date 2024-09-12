16627c0b75ae16ac86a528c557fe3e31f32b8581
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-12 17:20:01 +0500

    #2629 Runtime 94% Memory 95%

---
+++ 1/#028 fnComposition.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/function-composition/
+
+var compose = function (functions) {
+  return function (x) {
+    for (let i = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
+    }
+    return x;
+  };
+};
