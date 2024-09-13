fe62749aaa367f3cdc5e6b3c84eb52ba2dc40348
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-13 16:09:48 +0500

    #2798 Runtime 99% Memory 99%

--- 1/#34 targetHours.js
+++
@@ -1,5 +0,0 @@
-// https://leetcode.com/problems/number-of-employees-who-met-the-target/
-
-var numberOfEmployeesWhoMetTarget = function(hours, target) {
-    return hours.filter(hour=> hour>=target).length
-};
