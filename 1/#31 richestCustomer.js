f7877d9825bd66fc0b9f4a438e1cdd6b1494cb29
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-13 15:29:05 +0500

    #1672 Runtime 92% Memory 91%

--- 1/#31 richestCustomer.js
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/richest-customer-wealth/
-
-var maximumWealth = function(accounts) {
-    let res = accounts.map(account=>{
-        return account.reduce((sum,cur)=> sum+cur, 0 )
-        })
-    return Math.max(...res)
-};
