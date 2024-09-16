0ad7e41f4968f311b232de6a100ff59703a73343
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-09-16 15:13:26 +0500

    #1637 Runtime 89% Memory 94%

--- 1/#037 widestArea.js
+++ 1/#037 widestArea.js
@@ -1,9 +1,7 @@
 // https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
 
 var maxWidthOfVerticalArea = function (points) {
-  let sortedPoints = points.sort((a, b) => {
-    return b[0] - a[0];
-  });
+  let sortedPoints = points.sort((a, b) => b[0] - a[0]);
   let res = sortedPoints.map((num, i) => {
     if (sortedPoints.length > i + 1) return num[0] - sortedPoints[i + 1][0];
     else return 0;
